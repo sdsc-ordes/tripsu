4a83c53ad292e0470fd312106bf55a0b0b55ba4e
cmdoret <cyril.mattheydoret@gmail.com>
2024-06-19 18:26:37 +0200

    refactor(model): drop MaskedTriple, use Triple + TripleMask

--- src/model.rs
+++ src/model.rs
@@ -1,70 +1,71 @@
-use std::fmt::Debug;
-use bitflags::bitflags;
 use rio_api::model::{Subject, Term, Triple};
 
-
-bitflags! {
-    #[derive(Debug)]
-    pub struct TripleMask: u8 {
-        const SUBJECT = 1 << 0;
-        const OBJECT = 1 << 1;
-    }
-}
-
-impl TripleMask {
-    // Checks if a all bits in `mask` are set.
-    fn is_set(&self, mask: TripleMask) -> bool {
-        return self.bits() & mask.bits() == mask.bits();
-    }
-}
 
 pub trait Pseudonymize {
     fn pseudo(&self) -> Self;
 }
 
-#[derive(Debug)]
-pub(crate) struct MaskedTriple<'a> {
-    triple: Triple<'a>,
-    mask: TripleMask,
+// Represent an individual component of a triple.
+#[repr(u8)]
+pub enum TriplePart {
+    SUBJECT   = 0b100,
+    PREDICATE = 0b010,
+    OBJECT    = 0b001,
 }
 
-impl MaskedTriple<'_> {
-    pub fn new(triple: Triple, mask: TripleMask) -> MaskedTriple{
-        MaskedTriple{ triple, mask}
+// Used to select any combination of fields in a triple
+pub struct TripleMask(u8);
+
+impl TripleMask {
+    pub fn new() -> Self {
+        return TripleMask(0);
     }
+
+    pub fn union(&mut self, other: TripleMask) -> TripleMask {
+        return TripleMask(self.0 | other.0)
+    }
+
+    pub fn is_set(&self, part: TriplePart) -> bool {
+        return (self.0 & part as u8) != 0
+    }
+
+    pub fn bits(&self) -> u8 {
+        return self.0
+    }
+
+    pub fn set(&mut self, part: TriplePart) {
+        self.0 |= part as u8;
+    }
+
 }
 
 // Pseudonymize parts of a triple set by its mask
-impl<'a> Pseudonymize for MaskedTriple<'a> {
-    fn pseudo(self: &MaskedTriple<'a>) -> MaskedTriple<'a> {
-        let pseudo_subject = if self.mask.is_set(TripleMask::SUBJECT) {
-            &self.triple.subject.pseudo()
-        } else {
-            &self.triple.subject.clone()
-        };
+pub fn pseudonymize_triple<'a>(triple: &Triple<'a>, mask: TripleMask) -> Triple<'a> {
+    let pseudo_subject = if mask.is_set(TriplePart::SUBJECT) {
+        &triple.subject.pseudo()
+    } else {
+        &triple.subject.clone()
+    };
 
-        let pseudo_object = if self.mask.is_set(TripleMask::OBJECT) {
-            self.triple.object.pseudo()
-        } else {
-            self.triple.object.clone()
-        };
-        return MaskedTriple::new(
-            Triple {
-                subject: *pseudo_subject,
-                predicate: self.triple.predicate,
-                object: pseudo_object,
-            }, TripleMask::SUBJECT
-        )
-    }
+    let pseudo_object = if mask.is_set(TriplePart::OBJECT) {
+        triple.object.pseudo()
+    } else {
+        triple.object.clone()
+    };
+    return Triple {
+            subject: *pseudo_subject,
+            predicate: triple.predicate,
+            object: pseudo_object,
+        }
 }
 
 // Pseudonymization of objects (Nodes or literals)
 impl Pseudonymize for Term<'_> {
     fn pseudo(&self) -> Self{
         match self {
-            Term::Literal(val) => return Term::Literal(*val),
-            Term::NamedNode(val) => return Term::NamedNode(*val),
-            Term::BlankNode(val) => return Term::BlankNode(*val),
+            Term::Literal(val) => Term::Literal(*val),
+            Term::NamedNode(val) => Term::NamedNode(*val),
+            Term::BlankNode(val) => Term::BlankNode(*val),
             Term::Triple(_) => panic!("RDF-star not supported (triple as object)"),
         }
     }
@@ -74,8 +75,8 @@ impl Pseudonymize for Term<'_> {
 impl Pseudonymize for Subject<'_> {
     fn pseudo(&self) -> Self{
         match self {
-            Subject::NamedNode(val) => return Subject::NamedNode(*val),
-            Subject::BlankNode(val) => return Subject::BlankNode(*val),
+            Subject::NamedNode(val) => Subject::NamedNode(*val),
+            Subject::BlankNode(val) => Subject::BlankNode(*val),
             Subject::Triple(_) => panic!("RDF-star not supported (triple as subject)"),
         }
     }
