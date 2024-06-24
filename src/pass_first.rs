341de0e01c54535480fd4f7ea63eabba12a6e68e
Cyril Matthey-Doret <cyril.mattheydoret@gmail.com>
2024-06-24 10:57:59 +0200

    feat: create and load index (#17)
    Co-authored-by: Gabriel Nützi <gnuetzi@gmail.com>

--- src/pass_first.rs
+++ src/pass_first.rs
@@ -1,1 +1,27 @@
+use rio_api::{model::Triple, parser::TriplesParser};
+use rio_turtle::TurtleError;
+use std::{io::Write, path::Path};
 
+use crate::io;
+
+fn index_triple(t: Triple, out: &mut impl Write) -> Result<(), TurtleError> {
+    match t.predicate.iri {
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" => {
+            let _ = out.write(&format!("{} .\n", &t.to_string()).into_bytes());
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
+
+pub fn create_type_map(input: &Path, output: &Path) {
+    let buf_in = io::get_reader(input);
+    let mut buf_out = io::get_writer(output);
+    let mut triples = io::parse_ntriples(buf_in);
+    while !triples.is_end() {
+        triples
+            .parse_step(&mut |t| index_triple(t, &mut buf_out))
+            .unwrap();
+    }
+}
