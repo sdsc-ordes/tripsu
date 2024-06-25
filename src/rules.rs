5071dbe744357f81e25af10239264fe4ba7fb154
supermaxiste <supermegaiperste@hotmail.com>
2024-06-25 11:11:14 +0200

    Update src/rules.rs
    Co-authored-by: Cyril Matthey-Doret <cyril.mattheydoret@gmail.com>

--- src/rules.rs
+++ src/rules.rs
@@ -4,7 +4,7 @@ use::std::collections::HashMap;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     // Replace values of nodes with a certain type.
-    pub replace_values_of_nodes_with_type: Vec<String>,
+    pub replace_uri_of_nodes_with_type: Vec<String>,
 
     // Replace values of `subject` & `predicate`.
     pub replace_values_of_subject_predicate: HashMap<String, Vec<String>>,
