41eff3ac632f2f5819aa1674eb35f8e5ba1b9ec0
Gabriel Nützi <gnuetzi@gmail.com>
2024-07-09 11:02:31 +0200

    fix: WIP
    Signed-off-by: Gabriel Nützi <gnuetzi@gmail.com>

--- tools/ci/assert-tag.sh
+++ tools/ci/assert-tag.sh
@@ -15,41 +15,42 @@ cd "$ROOT_DIR"
 RELEASE_BRANCH="main"
 
 function main() {
-    local push="false"
-    [ "$1" = "--push" ] && shift 1 && push="true"
-
-    local prepare_tag="$1"
+    local type="$1"
+    local prepare_tag="$2"
     local release_tag=${prepare_tag##*prepare-}
 
-    if [ "$push" = "true" ]; then
+    if [ "$type" = "push" ]; then
         print_info "Pushing tag '$release_tag'."
         git push origin "$release_tag" ||
             die "Could not push tag."
 
         exit 0
+    elif [ "$type" = "create-and-check" ]; then
+        print_info "Create tag '$release_tag' and check."
+
+        # Gets the message on the annotated commit:
+        deref() {
+            git for-each-ref "refs/tags/$release_tag" --format="%($1)"
+        }
+
+        deref contents
+
+        # Creates a new tag with the same message,
+        # including trailing headers.
+        git tag -a -m "$(deref contents)" "$release_tag" ||
+            die "Could not create tag."
+
+        # Fetch the branch.
+        git fetch --depth 50 origin "$RELEASE_BRANCH"
+
+        # Check if its reachable.
+        if [ -n "$(git rev-list --first-parent \
+            --ancestry-path \
+            "$release_tag^..origin/$RELEASE_BRANCH")" ]; then
+            die "Tag is not reachable from '$RELEASE_BRANCH' (--first-parent) !"
+        fi
     fi
 
-    # Gets the message on the annotated commit:
-    deref() {
-        git for-each-ref "refs/tags/$release_tag" --format="%($1)"
-    }
-
-    deref contents
-
-    # Creates a new tag with the same message,
-    # including trailing headers.
-    git tag -a -m "$(deref contents)" "$release_tag" ||
-        die "Could not create tag."
-
-    # Fetch the branch.
-    git fetch --depth 50 origin "$RELEASE_BRANCH"
-
-    # Check if its reachable.
-    if [ -n "$(git rev-list --first-parent \
-        --ancestry-path \
-        "$release_tag^..origin/$RELEASE_BRANCH")" ]; then
-        die "Tag is not reachable from '$RELEASE_BRANCH' (--first-parent) !"
-    fi
 }
 
 main "$@"
