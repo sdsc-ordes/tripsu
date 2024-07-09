c8c81750906c9e5c682bf780c6db826498ea7307
Gabriel Nützi <gnuetzi@gmail.com>
2024-07-09 17:36:40 +0200

    fix: add opencontainers label on image build
    Signed-off-by: Gabriel Nützi <gnuetzi@gmail.com>

--- tools/nix/images/rdf-protect.nix
+++ tools/nix/images/rdf-protect.nix
@@ -11,5 +11,10 @@ pkgs.dockerTools.buildLayeredImage {
   config = {
     Entrypoint = ["rdf-protect"];
     WorkingDir = "/";
+    Labels = {
+      "org.opencontainers.image.source" = "https://github.com/sdsc-ordes/rdf-protect";
+      "org.opencontainers.image.description" = rdf-protect.meta.description;
+      "org.opencontainers.image.license" = "Apache-2.0";
+    };
   };
 }
