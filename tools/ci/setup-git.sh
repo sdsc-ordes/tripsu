4c47b1010e8f33546f4a78cbad9ec4d5288a0229
Gabriel Nützi <gnuetzi@gmail.com>
2024-07-17 14:59:54 +0200

    fix: correct strange HOME overwriting

--- tools/ci/setup-git.sh
+++ tools/ci/setup-git.sh
@@ -13,3 +13,12 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 . "$ROOT_DIR/tools/general.sh"
 
 ci_setup_git
+
+# Some home workaround for this issue:
+# https://github.com/actions/runner/issues/863
+# Why, really whyyyy is Github overwriting the HOME directory!
+if [ "$HOME" = "/github/home" ]; then
+    print_warning "Making symlink for '$HOME' to /root"
+    mkdir -p /github
+    ln -s /root "$HOME"
+fi
