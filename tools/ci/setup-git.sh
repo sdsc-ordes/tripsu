6e96a37b7780bf145d9e75fe8ac8287b1b9b2078
Gabriel Nützi <gnuetzi@gmail.com>
2024-07-17 15:03:10 +0200

    fix: test

--- tools/ci/setup-git.sh
+++ tools/ci/setup-git.sh
@@ -12,13 +12,17 @@ git config --global safe.directory "*" || {
 ROOT_DIR=$(git rev-parse --show-toplevel)
 . "$ROOT_DIR/tools/general.sh"
 
-ci_setup_git
-
 # Some home workaround for this issue:
 # https://github.com/actions/runner/issues/863
 # Why, really whyyyy is Github overwriting the HOME directory!
 if [ "$HOME" = "/github/home" ]; then
-    print_warning "Making symlink for '$HOME' to /root"
+    print_warning "Making symlink for '$HOME' to /root to" \
+        "workaround some Github stupidity."
+
+    ls -al "/github" || true
     mkdir -p /github
     ln -s /root "$HOME"
+    ls -al "/github"
 fi
+
+ci_setup_git
