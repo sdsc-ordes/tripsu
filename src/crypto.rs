9131f262dc64c4575382c859afe5de00129d2717
cmdoret <cyril.mattheydoret@gmail.com>
2024-07-22 16:46:10 +0200

    refactor(crypto): minimize blanket impl in Pseudonymize

--- src/crypto.rs
+++ src/crypto.rs
@@ -9,6 +9,10 @@ pub(crate) fn generate_key(size: usize) -> Vec<u8> {
 }
 
 pub trait Pseudonymize {
+    
+    // implementers need to define raw bytes pseudonymization
+    fn pseudo(&self, input: &[u8]) -> String; 
+
     // Pseudonymize parts of a triple set by its mask
     fn pseudo_triple(&self, triple: &Triple, mask: TripleMask) -> Triple {
         let pseudo_subject = if mask.is_set(&TripleMask::SUBJECT) {
@@ -27,7 +31,7 @@ pub trait Pseudonymize {
             subject: Subject::from(pseudo_subject.clone()),
             predicate: triple.predicate.clone(),
             object: Term::from(pseudo_object.clone()),
-        };
+        }
     }
 
     fn pseudo_entity(&self, e: &Entity) -> Entity {
@@ -38,44 +42,14 @@ pub trait Pseudonymize {
         }
     }
     // private methods? Blanket implementations
-    fn pseudo_named_node(&self, t: &NamedNode) -> NamedNode;
-    //return t.clone();
-
-    fn pseudo_literal(&self, l: &Literal) -> Literal;
-    //return l.clone();
-
-    fn pseudo_blank_node(&self, u: &BlankNode) -> BlankNode;
-    // return u.clone()
-}
-
-#[derive(Default)]
-pub struct DefaultHasher {
-    // Key used for hashing
-    pub key: [u8; 32],
-}
-impl DefaultHasher {
-    pub fn new(key: Vec<u8>) -> Self {
-        // convert the key to a [u8, 32]
-        let mut new_key = [0; 32];
-        // make sure that 
-        // 1) the key is not longer than 32 bytes
-        // 2) the key is not shorter than 32 bytes
-        // To do: make sure that the loop below works
-        for i in 0..32 {
-            new_key[i] = key[i];
-        }
-        return DefaultHasher { key: new_key };
-    }
-}
-
-impl Pseudonymize for DefaultHasher {
     fn pseudo_named_node(&self, t: &NamedNode) -> NamedNode {
-        // We check for the last backslash in the IRI and add 1 to include the backslash
-        let prefix = &t.iri[0..t.iri.rfind('/').unwrap() + 1];
-        let hash = blake3::keyed_hash(self.key, t.iri.as_bytes()).to_string();
+        // We check for the last fragment or path separator in the IRI
+        let prefix_end= t.iri.rfind(|c: char| (c == '/') || (c == '#')).unwrap();
+        let prefix = &t.iri[0..=prefix_end];
+        let crypted = self.pseudo(t.iri.as_bytes()).to_string();
         return NamedNode {
-            iri: format!("{prefix}{hash}"),
-        };
+            iri: format!("{prefix}{crypted}"),
+        }
     }
 
     fn pseudo_literal(&self, l: &Literal) -> Literal {
@@ -84,9 +58,9 @@ impl Pseudonymize for DefaultHasher {
             Literal::LanguageTaggedString { value, language: _ } => value,
             Literal::Simple { value } => value,
         };
-        let hash = blake3::hash(value.as_bytes());
+        let crypted = self.pseudo(value.as_bytes());
         return Literal::Simple {
-            value: hash.to_string(),
+            value: crypted,
         };
     }
 
