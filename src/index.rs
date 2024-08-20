4cdbe7921829a49bf7ce0aa4cb6211bb40275b63
cmdoret <cyril.mattheydoret@gmail.com>
2024-08-20 11:43:29 +0200

    feat(index): add dedicated struct+impl

--- src/index.rs
+++ src/index.rs
@@ -1,11 +1,56 @@
 use rio_api::parser::TriplesParser;
 use rio_turtle::TurtleError;
 use std::{io::Write, path::Path};
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     io,
     rdf_types::{Triple, TripleView},
 };
+
+#[derive(Serialize, Deserialize)]
+struct Index {
+    types: Vec<String>,
+    map: HashMap<[u8; 8], usize>,
+
+    #[serde(skip)]
+    hasher: DefaultHasher,
+}
+
+impl Index {
+
+    fn hash(&mut self, s: &str) -> [u8; 8] {
+        s.hash(&mut self.hasher);
+        self.hasher.finish().to_be_bytes()
+    }
+
+    fn new() -> Self {
+        Index {
+            types: Vec::new(),
+            map: HashMap::new(),
+            hasher: DefaultHasher::new(),
+        }
+    }
+
+    fn insert(&mut self, subject_key: &str, type_val: &str) {
+        let key = self.hash(subject_key);
+        let type_idx: usize;
+        if self.types.contains(&type_val.to_string()) {
+            type_idx = self.types.iter().position(|x| *x == type_val).unwrap();
+        } else {
+            type_idx = self.types.len();
+            self.types.push(type_val.to_string());
+        }
+        self.map.insert(key, type_idx);
+    }
+
+    fn get(&mut self, subject_key: &str) -> Option<&str> {
+        let key = self.hash(subject_key);
+        self.map.get(&key).map(|i| self.types[*i].as_str())
+    }
+}
 
 fn index_triple(t: Triple, out: &mut impl Write) {
     if t.predicate.iri.as_str() == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" {
@@ -24,6 +69,7 @@ pub fn create_type_map(input: &Path, output: &Path) {
     let buf_in = io::get_reader(input);
     let mut buf_out = io::get_writer(output);
     let mut triples = io::parse_ntriples(buf_in);
+
 
     while !triples.is_end() {
         let _ = triples
