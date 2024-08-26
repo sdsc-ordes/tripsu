56fc66ba362f8439378f254dd3c6d8dc58822e75
cmdoret <cyril.mattheydoret@gmail.com>
2024-08-27 01:29:32 +0200

    refactor(bench): function-based benchmark script

--- tools/bench/benchmark.sh
+++ tools/bench/benchmark.sh
@@ -68,43 +68,93 @@ objects:
 EOF
 
 ### Commands to benchmark
-
 BASE_CMD_IDX="${BASE_BIN} index -o ${BASE_IDX} ${INPUT}"
 COMP_CMD_IDX="${COMP_BIN} index -o ${COMP_IDX} ${INPUT}"
 BASE_CMD_PSD="${BASE_BIN} pseudo -r ${RULES} -x ${BASE_IDX} ${INPUT}"
 COMP_CMD_PSD="${COMP_BIN} pseudo -r ${RULES} -x ${COMP_IDX} ${INPUT}"
 
-### Profile time
+### functions for profiling
 
-HYPF_OPTS=( -r 2 )
-HYPF_IDX_OUT=$(mktemp)
-HYPF_PSD_OUT=$(mktemp)
-
-# indexing
-hyperfine --export-markdown "${HYPF_IDX_OUT}" "${HYPF_OPTS[@]}" \
-    -n "${BASE_BRANCH}" "${BASE_CMD_IDX}" \
-    -n "${COMP_BRANCH}" "${COMP_CMD_IDX}"
-# pseudonymization
-hyperfine --export-markdown "${HYPF_PSD_OUT}" "${HYPF_OPTS[@]}" \
-    -n "${BASE_BRANCH}" "${BASE_CMD_PSD}" \
-    -n "${COMP_BRANCH}" "${COMP_CMD_PSD}"
-
-
-### Profile memory
-
-HEAP_IDX_OUT=$(mktemp)
-HEAP_PSD_OUT=$(mktemp)
+cpu_prof() {
+    local branch1=$1
+    local cmd1=$2
+    local branch2=$3
+    local cmd2=$4
+    local out=$5
+    hyperfine --export-markdown "${out}" -r 2 \
+        -n "${branch1}" "${cmd1}" \
+        -n "${branch2}" "${cmd2}"
+}
 
 mem_prof() {
     local name=$1
     local cmd=$2
-    local HEAP_OUT
-    HEAP_OUT=$(mktemp)
+    local heap_out
+    heap_out=$(mktemp)
     echo -n "$name: "
-    heaptrack -o "${HEAP_OUT}" ${cmd} >/dev/null
-    heaptrack_print "${HEAP_OUT}.zst" \
+    # shellcheck disable=SC2086
+    heaptrack -o "${heap_out}" ${cmd} >/dev/null
+    heaptrack_print "${heap_out}.zst" \
     | grep '^peak heap memory'
 }
+
+make_report() {
+    local cpu_index=$1
+    local cpu_pseudo=$2
+    local mem_index=$3
+    local mem_pseudo=$4
+    local base_branch=$5
+
+    cat <<-MD
+	# tripsu profiling
+
+	> date: $(date -u +%Y-%m-%d)
+
+    Comparing $(git branch --show-current) against $base_branch.
+	
+	## Timings
+	
+	Run time compared using hyperfine
+	
+	### Indexing
+	
+	$(cat "${cpu_index}")
+	
+	### Pseudonymization
+	
+	$(cat "${cpu_pseudo}")
+	
+	## Memory
+	
+	Heap memory usage compared using heaptrack
+	
+	### Indexing
+	
+	$(cat "${mem_index}")
+	
+	### Pseudonymization
+	
+	$(cat "${mem_pseudo}")
+	MD
+}
+
+
+###  Run profiling
+
+## Profile cpu time
+HYPF_IDX_OUT=$(mktemp)
+HYPF_PSD_OUT=$(mktemp)
+
+# indexing
+cpu_prof "${BASE_BRANCH}" "${BASE_CMD_IDX}" \
+         "${COMP_BRANCH}" "${COMP_CMD_IDX}" "${HYPF_IDX_OUT}"
+# pseudonymization
+cpu_prof "${BASE_BRANCH}" "${BASE_CMD_IDX}" \
+         "${COMP_BRANCH}" "${COMP_CMD_IDX}" "${HYPF_PSD_OUT}"
+
+## Profile memory
+HEAP_IDX_OUT=$(mktemp)
+HEAP_PSD_OUT=$(mktemp)
 
 # indexing
 mem_prof "${BASE_BRANCH}" "${BASE_CMD_IDX}" >  "${HEAP_IDX_OUT}"
@@ -113,36 +163,9 @@ mem_prof "${COMP_BRANCH}" "${COMP_CMD_IDX}" >> "${HEAP_IDX_OUT}"
 mem_prof "${BASE_BRANCH}" "${BASE_CMD_PSD}" >  "${HEAP_PSD_OUT}"
 mem_prof "${COMP_BRANCH}" "${COMP_CMD_PSD}" >> "${HEAP_PSD_OUT}"
 
+
 ### Reporting
-
-cat << MD > ${OUTPUT}
-# tripsu profiling
-
-> date: $(date -u +%Y-%m-%d)
-
-Comparing branch XXX against YYY.
-
-## Timings
-
-Run time compared using hyperfine
-
-### Indexing
-
-$(cat "${HYPF_IDX_OUT}")
-
-### Pseudonymization
-
-$(cat "${HYPF_PSD_OUT}")
-
-## Memory
-
-Heap memory usage compared using heaptrack
-
-### Indexing
-
-$(cat "${HEAP_IDX_OUT}")
-
-### Pseudonymization
-
-$(cat "${HEAP_PSD_OUT}")
-MD
+make_report \
+    "${HYPF_IDX_OUT}" "${HYPF_PSD_OUT}" \
+    "${HEAP_IDX_OUT}" "${HEAP_PSD_OUT}" 
+    "${BASE_BRANCH}" > "${OUTPUT}"
