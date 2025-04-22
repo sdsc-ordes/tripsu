2880da2bd4256b0485652125c64e7a2ebf49329f
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2025-04-22 08:57:15 +0200

    fix: minimal toolchain

--- tools/nix/flake.nix
+++ tools/nix/flake.nix
@@ -57,7 +57,7 @@
           pkgs = import inputs.nixpkgs { inherit system overlays; };
 
           # Set the rust toolchain from the `rust-toolchain.toml`.
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ../../rust-toolchain.toml;
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ../configs/rust/toolchain-release.toml;
 
           # Basic Packages.
           nativeBuildInputsBasic = [
