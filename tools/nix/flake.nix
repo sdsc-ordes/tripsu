d4587d0c3eaafb9629ebc75a9cf7925cdf95fd6d
Gabriel Nützi <gnuetzi@gmail.com>
2025-05-05 14:07:58 +0200

    chore(ci): only use extra substituters (#59)

--- tools/nix/flake.nix
+++ tools/nix/flake.nix
@@ -2,19 +2,14 @@
   description = "tripsu";
 
   nixConfig = {
-    substituters = [ "https://cache.nixos.org" ];
-    extra-substituters = [
+    extra-trusted-substituters = [
+      "https://cache.nixos.org"
       "https://tripsu.cachix.org"
-      # Nix community's cache server
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "tripsu.cachix.org-1:pWZmirIwlMxGMVWSDMjQm4R+zLp8gtaT8OfH0Sv/j4E="
-    ];
-    extra-trusted-substituters = [
-      "https://tripsu.cachix.org"
-      "https://cache.nixos.org"
     ];
   };
 
